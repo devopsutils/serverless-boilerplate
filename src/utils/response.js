3b99fa7e4acb0a8e59bbc7168629f69941fec14d
Marian Craciun <mariancraciun1983@gmail.com>
2019-10-02 23:10:08 +0300

    Fixed CORB, removed sls tests from travis testing

--- src/utils/response.js
+++ src/utils/response.js
@@ -1,9 +1,15 @@
 module.exports.json = (data = null, status = 200) => ({
   statusCode: status,
+  headers: {
+    'Access-Control-Allow-Origin': '*' // CORB
+  },
   body: JSON.stringify(data)
 });
 
 module.exports.error = (message = 'Error', status = 500) => ({
   statusCode: status,
+  headers: {
+    'Access-Control-Allow-Origin': '*' // CORB
+  },
   body: JSON.stringify(message)
 });
